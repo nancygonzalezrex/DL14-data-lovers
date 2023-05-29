73b003610d58afc0e7fb514d2e4b7309cae6854a
Danitap-97 <131627619+Danitap-97@users.noreply.github.com>
2023-05-29 11:59:05 -0500

    se agrega parrafo mas boton

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
+const boton=document.getElementById('btn')
+
+boton.addEventListener('click', (e) => {
+    e.preventDefault();
+})
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(data.films);
+console.log(boton);
