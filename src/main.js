2fafd6ce24ac4e22e8731ce444ed53b6aeb65c0d
Danitap-97 <131627619+Danitap-97@users.noreply.github.com>
2023-06-03 07:22:28 -0500

    se crea condicional para las pantallas

--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
 import { mostrarPeliculas } from './data.js';
+const primeraPantalla = document.getElementById("primera-pantalla");
+const segundaPantalla = document.getElementById("segunda-pantalla");
 const peliculas = mostrarPeliculas();
 console.log(peliculas);
 
-function verPeliculas (dataghibli) {
-    const contenedor =document.getElementById("contenedor-peliculas");
-    for (let i = 0; i < dataghibli.length; i++) {
-        const listaPeliculas = `<li><h1></h1> ${peliculas[i].title}  <img src =${peliculas[i].poster}>  </img> ${peliculas[i].description}</li>`;
-        contenedor.innerHTML += listaPeliculas;
-    }
+function verPeliculas(dataghibli) {
+  const contenedor = document.getElementById("contenedor-peliculas");
+  for (let i = 0; i < dataghibli.length; i++) {
+    const listaPeliculas = `<li><h1></h1> ${peliculas[i].title}  <img src =${peliculas[i].poster}>  </img> ${peliculas[i].description}</li>`;
+    contenedor.innerHTML += listaPeliculas;
+  }
 }
 verPeliculas(peliculas);
-const boton=document.getElementById('btn')
-
-boton.addEventListener('click', (e) => 
-     e.preventDefault());
+//se crea escuchador de evento para el boton
+const boton = document.getElementById('btn')
+boton.addEventListener('click',  (e) =>
+  e.preventDefault())
+if (primeraPantalla) {
+  primeraPantalla.style.display = "block";
+  segundaPantalla.style.display = "none";
+} else {
+  segundaPantalla.style.display = "block";
+  primeraPantalla.style.display = "none";
+}
+        
+     
      
    /* const studio = films
  data.filter(function(pelicula){
