b5c544a273647fcb58463d4f293a370fd19dbd2e
nancygonzalezrex <nancygonzalezrex@gmail.com>
2023-06-06 18:55:14 -0400

    Modifique pagina principal

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-import { mostrarPeliculas } from './data.js';
+import { mostrarPeliculas } from "./data.js";
 const peliculas = mostrarPeliculas();
 console.log(peliculas);
 
-function verPeliculas (dataghibli) {
-    const contenedor =document.getElementById("contenedor-peliculas");
-    for (let i = 0; i < dataghibli.length; i++) {
-        const listaPeliculas = `<li><h1></h1> ${peliculas[i].title}  <img src =${peliculas[i].poster}>  </img> ${peliculas[i].description}</li>`;
-        contenedor.innerHTML += listaPeliculas;
-    }
+function verPeliculas(dataghibli) {
+  const contenedor = document.getElementById("contenedor-peliculas");
+  for (let i = 0; i < dataghibli.length; i++) {
+    const listaPeliculas = `
+    <li class="pelicula">
+      <h2>${peliculas[i].title}</h2>
+      <img src =${peliculas[i].poster} class="imagen-pelicula" />
+      <p>${peliculas[i].description}</p>
+    </li>`;
+    contenedor.innerHTML += listaPeliculas;
+  }
 }
 verPeliculas(peliculas);
-const boton=document.getElementById('btn')
+const boton = document.getElementById("btn");
 
-boton.addEventListener('click', (e) => 
-     e.preventDefault());
-     
-   /* const studio = films
+boton.addEventListener("click", (e) => e.preventDefault());
+
+/* const studio = films
  data.filter(function(pelicula){
     return pelicula.title === 'title'
  })
