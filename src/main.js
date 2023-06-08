b006c44164005371c17dfc1db63a8d2b2563e98f
nancygonzalezrex <nancygonzalezrex@gmail.com>
2023-06-08 19:35:41 -0400

    se creo funcion sort

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import { mostrarPeliculas } from './data.js';
-const botonIr = document.getElementById('btn-ir');
-const botonVolver = document.getElementById('btn-volver');
+import { mostrarPeliculas } from "./data.js";
+const botonIr = document.getElementById("btn-ir");
+const botonVolver = document.getElementById("btn-volver");
 const peliculas = mostrarPeliculas();
-console.log(peliculas);
 
 function verPeliculas(dataghibli) {
+  const verOrden = dataghibli.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+  verPeliculas(verOrden)
   const contenedor = document.getElementById("contenedor-peliculas");
   for (let i = 0; i < dataghibli.length; i++) {
     const listaPeliculas = `
@@ -18,33 +29,19 @@ function verPeliculas(dataghibli) {
 }
 verPeliculas(peliculas);
 //se crea escuchador de evento para los botones
-botonIr.addEventListener('click', alternarPantallas);
-botonVolver.addEventListener('click', alternarPantallas);
+botonIr.addEventListener("click", alternarPantallas);
+botonVolver.addEventListener("click", alternarPantallas);
 
 function alternarPantallas(e) {
-  console.log(e)
   e.preventDefault();
   const header = document.getElementById("header");
   const segundaPantalla = document.getElementById("segunda-pantalla");
 
-  if (window.getComputedStyle(header).display == "block") {
+  if (window.getComputedStyle(header).display === "block") {
     header.style.display = "none";
     segundaPantalla.style.display = "block";
-  } else if (window.getComputedStyle(segundaPantalla).display == "block") {
+  } else if (window.getComputedStyle(segundaPantalla).display === "block") {
     segundaPantalla.style.display = "none";
     header.style.display = "block";
   }
 }
-
-/*const filtrarData = function (){
-  peliculas.filter(pelicula => pelicula.title);
-}
-console.log(filtrarData);
-function filtrarInformacion(data){
-const ficha = document.getElementById("card-fichaTecnica");
-for (let i = 0;i< data.length; i++) {
-   const fichaTecnica = `<div>${peliculas[i].title} <br> ${peliculas[i].director} <br> ${peliculas[i].producer} <br> ${peliculas[i].release_date}</div>`;
-   ficha.innerHTML += fichaTecnica;
-}
-}
-filtrarInformacion(peliculas);*/
